4867c4708b8f4427a658b1f71995826ca0fbe605
zhanghang <zhanghang@uinoa.com>
2021-11-17 18:23:06 +0800

    fix: config

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,7 @@
  * @Email: suchiva@126.com
  * @Date: 2021-11-16 09:28:37
  * @LastEditors: zhanghang
- * @LastEditTime: 2021-11-17 18:18:03
+ * @LastEditTime: 2021-11-17 18:22:40
  */
 export default [
   {
