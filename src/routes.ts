59aed61779ce4eb7d779a147e6cc12ae3ac197a9
zhanghang <zhanghang@uinoa.com>
2021-11-19 18:22:26 +0800

    fix: 修改查看和编辑交互方式

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,7 @@
  * @Email: suchiva@126.com
  * @Date: 2021-11-16 09:28:37
  * @LastEditors: zhanghang
- * @LastEditTime: 2021-11-19 18:19:44
+ * @LastEditTime: 2021-11-19 18:21:03
  */
 export default [
   {
