c4f136050191bb3aae7868b017b40d6fe9ad92b7
zhanghang <zhanghang@uinoa.com>
2021-12-02 16:12:03 +0800

    fix: 路由配置汉化

---
+++ src/layouts/navmap.tsx
@@ -0,0 +1,19 @@
+/*
+ * @Description:
+ * @Version: 2.0
+ * @Autor: zhanghang
+ * @Email: suchiva@126.com
+ * @Date: 2021-12-02 15:36:33
+ * @LastEditors: zhanghang
+ * @LastEditTime: 2021-12-02 16:09:29
+ */
+export const navMap: any = {
+  sysManager: '系统管理',
+  roles: '角色管理',
+  users: '用户管理',
+  user: '用户管理',
+  mini: '小程序',
+  feedback: '反馈列表',
+  form: '表单',
+  detail: '详情',
+};
